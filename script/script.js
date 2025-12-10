9286341fcaaf9a6f5cc6049a1f49add31d2d4285
Thomas Többe <toebbe.thomas@outlook.de>
2025-12-10 16:14:40 +0100

    feat: first fetch to consol.table and data on index.html

---
+++ script/script.js
@@ -0,0 +1,60 @@
+const API_URL = "https://pokeapi.co/api/v2/pokemon/";
+
+const API_URL_LIMITED = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
+
+function onloadInit() {
+  loadLocalData();
+}
+
+async function loadLocalData() {
+  let isLoading = true; // Ladezustand setzen für Ladeanzeige
+  pokedexData = await fetch(API_URL_LIMITED)
+    .then((response) => response.json()) // konvertiere Antwort in JSON
+    .then((data) => data.results) // extrahiere das results-Array
+    .catch((error) => console.error("Error loading local data:", error)) // protokolliere alle Fehler
+    .finally(() => {
+      // wird immer ausgeführt, egal ob Erfolg oder Fehler / erfolgt erst nach allen then/catch
+      isLoading = false; // Ladezustand zurücksetzen
+    });
+  console.table(pokedexData);
+  loadPokemonDetails(); // Rufe die Funktion zum Laden der Pokémon-Details auf
+}
+
+async function loadPokemonDetails() {
+  isLoading = true; // Ladezustand setzen für Ladeanzeige
+  for (let i = 0; i < pokedexData.length; i++) {
+    let pokemon = pokedexData[i];
+    let details = await fetchPokemonDetails(pokemon.url); //Achtung, hier iterieren wir über jedes Pokémon und holen die Details
+    pokedexData[i] = { ...pokemon, details }; // Füge die Details zum Pokémon-Objekt hinzu, durch die dreipunkt-Syntax wird ein neues Objekt erstellt
+  }
+  console.table(pokedexData); // Protokolliere die aktualisierten Daten mit Details
+  renderPokedexListView(); // Rufe die Funktion zum Rendern der Liste auf
+  isLoading = false; // Ladezustand zurücksetzen
+}
+
+async function fetchPokemonDetails(url) { //Details eines einzelnen Pokémons holen
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error("Error fetching pokemon details:", error));
+}
+
+function renderPokedexListView() {
+  let listContainer = document.getElementById("pokedex-container");
+  listContainer.innerHTML = ""; // Leere den Container vor dem Rendern
+
+  pokedexData.forEach((pokemon, index) => {
+    // Iteriere über jedes Pokémon im Array
+    let listItem = document.createElement("div"); // Erstelle ein neues Div-Element für jedes Pokémon
+    listItem.className = "pokedex-item"; // Füge eine Klasse für Styling hinzu
+    //Bild und Namen
+    //listItem.innerText = `${index + 1}. ${pokemon.name}`; // Setze den Text des Elements auf den Namen des Pokémon
+
+    //HTML-Inhalt mit Bild und Name
+    listItem.innerHTML = `  
+      <img src="${pokemon.details.sprites.front_default}" alt="${pokemon.name}">
+      <p>${index + 1}. ${pokemon.name}</p>
+    `;
+
+    listContainer.appendChild(listItem); // Füge das Element dem Container hinzu
+  });
+}
