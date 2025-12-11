1d459f3b2adeba2601bba24a69bc6b76ea5ee12d
Thomas Többe <toebbe.thomas@outlook.de>
2025-12-11 21:52:52 +0100

    perf: minimal load for Listview

--- script/templates.js
+++ script/templates.js
@@ -25,41 +25,26 @@ function renderPokedexListView() {
     html += /* html */ `
       <article
         class="pokedex-item"
-        id="pokemon-${p.details.id}"
+        id="pokemon-${p.id}"
         data-index="${i}"
         onclick="openPokemonModal(${i})"
       >
-        <img
-          class="pokemon-img"
-          src="${p.details.sprites.front_default}"
-          alt="${p.name}"
-        />
-        <h3 class="pokemon-title">#${i + 1} ${p.name}</h3>
+        <img class="pokemon-img" src="${p.sprite}" alt="${p.name}" />
+        <h3 class="pokemon-title">#${p.id} ${p.name}</h3>
         <div class="pokemon-types">
-          ${p.details.types
-            .map((t) => `<span class="type-badge">${t.type.name}</span>`)
+          ${p.abilities
+            .map((a) => `<span class="type-badge">${a}</span>`)
             .join("")}
-        </div>
-        <div class="pokemon-properties">
-          <div class="pokemon-property">
-            <span class="property-label">Height:</span
-            ><span class="property-value">${p.details.height / 10} m</span>
-          </div>
-          <div class="pokemon-property">
-            <span class="property-label">Weight:</span
-            ><span class="property-value">${p.details.weight / 10} kg</span>
-          </div>
         </div>
       </article>`;
   }
   listContainer.innerHTML = html;
 }
 
-
 //Öffnen des Modals mit Pokémon-Details
 function openPokemonModal(index) {
   const p = pokedexData[index];
-  currentPokemonIndex = index; // Aktualisiere den aktuellen Index
+  currentPokemonIndex = index;
   document.getElementById("modal-container").innerHTML = /* html */ `
     <div
       class="pokemon-modal-overlay"
@@ -67,41 +52,19 @@ function openPokemonModal(index) {
     >
       <div
         class="pokemon-modal-card"
-        id="pokemon-modal-${p.details.id}"
+        id="pokemon-modal-${p.id}"
         onclick="event.stopPropagation()"
       >
-        <img
-          class="modal-pokemon-img"
-          src="${p.details.sprites.other?.["official-artwork"]?.front_default ||
-          p.details.sprites.front_default}"
-          alt="${p.name}"
-        />
-        <h2 class="modal-pokemon-title">#${p.details.id} ${p.name}</h2>
+        <img class="modal-pokemon-img" src="${p.sprite}" alt="${p.name}" />
+        <h2 class="modal-pokemon-title">#${p.id} ${p.name}</h2>
         <div class="pokemon-types">
-          ${p.details.types
-            .map((t) => `<span class="type-badge">${t.type.name}</span>`)
+          ${p.abilities
+            .map((a) => `<span class="type-badge">${a}</span>`)
             .join("")}
-        </div>
-        <div class="pokemon-properties">
-          <div class="pokemon-property">
-            <span class="property-label">Height:</span
-            ><span class="property-value">${p.details.height / 10} m</span>
-          </div>
-          <div class="pokemon-property">
-            <span class="property-label">Weight:</span
-            ><span class="property-value">${p.details.weight / 10} kg</span>
-          </div>
-          <div class="pokemon-property">
-            <span class="property-label">Base XP:</span
-            ><span class="property-value">${p.details.base_experience}</span>
-          </div>
         </div>
         <div class="modal-button">
           <div>
-            <button
-              class="btn btn-primary"
-              onclick="previousPokemon()"
-            >
+            <button class="btn btn-primary" onclick="previousPokemon()">
               ⬅️
             </button>
           </div>
@@ -114,13 +77,10 @@ function openPokemonModal(index) {
             </button>
           </div>
           <div>
-            <button class="btn btn-primary" onclick="nextPokemon()">
-              ➡️
-            </button>
+            <button class="btn btn-primary" onclick="nextPokemon()">➡️</button>
           </div>
         </div>
       </div>
     </div>
   `;
 }
-
